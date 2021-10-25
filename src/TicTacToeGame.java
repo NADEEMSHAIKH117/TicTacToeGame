cc5be546b641642fe3839e2f9f618c4af557689d
NADEEMSHAIKH117 <nadeemshaikh1171998@gmail.com>
2021-10-25 19:47:31 +0530

    added UC-1

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,8 +1,18 @@
 
 public class TicTacToeGame {
+	public static final char EMPTY= ' ';
+	
+	
+	private static char[] createBoard() {
+		char [] ticTacToeBoard = new char[10];
+		for(int cellIndex = 1; cellIndex < ticTacToeBoard.length; cellIndex++) 
+			ticTacToeBoard[cellIndex] = EMPTY;
+		return ticTacToeBoard;
+	}
+
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
