68a160e22585846f50900bb3c662d2178bc55ab9
NADEEMSHAIKH117 <nadeemshaikh1171998@gmail.com>
2021-10-30 17:10:48 +0530

    UC-12 added

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -223,6 +223,63 @@ public class TicTacToeGame {
 			freeCentreOrSideIndex = 8;
 		return freeCentreOrSideIndex;
 	}
+	
+	/**
+	 * uc12
+	 */
+	private static void playGame() {
+		Scanner takeInput = new Scanner(System.in);
+		char[] ticTacToeBoard = createBoard();
+		System.out.println("Player letter: ");
+		char playerLetter = takeInput.next().charAt(0);
+		char computerLetter = selectLetter(playerLetter);
+		System.out.println("Computer Letter: " + computerLetter);
+		String currentPlayer = getWhoPlaysFirst();
+		char chosenLetter;
+		if(currentPlayer.contains("PLAYER"))
+			chosenLetter = playerLetter;
+		else
+			chosenLetter = computerLetter;
+		System.out.println("First Chance for " + currentPlayer);
+		System.out.println("\n--Initial status of board--");
+		showBoard(ticTacToeBoard);
+		String gameStatus;
+		int computerPlayReturnWin = 0, computerPlayReturnBlock = 0, computerFreeCornerAvailable = 0, computerFreeCentreOrSideAvailable = 0, moveIndex = 0;
+		do {
+			if(currentPlayer.contains(PlayerNames.COMPUTER.name())) {
+				computerPlayReturnWin = computerPlayToWin(ticTacToeBoard, chosenLetter);
+				computerPlayReturnBlock = computerPlayToBlock(ticTacToeBoard, chosenLetter);
+				computerFreeCornerAvailable = computerPlayToCorner(ticTacToeBoard);
+				computerFreeCentreOrSideAvailable = computerPlayToCentreOrSide(ticTacToeBoard);
+				if(computerPlayReturnWin != 0) 
+					moveIndex = computerPlayReturnWin;
+				else if(computerPlayReturnBlock != 0) {
+					moveIndex = computerPlayReturnBlock;
+					computerPlayReturnBlock = 0; 
+				}
+				else if(computerFreeCornerAvailable != 0) {
+					moveIndex = computerFreeCornerAvailable;
+					computerFreeCornerAvailable = 0; 
+				}
+				else if(computerFreeCentreOrSideAvailable != 0) {
+					moveIndex = computerFreeCentreOrSideAvailable;
+					computerFreeCentreOrSideAvailable = 0; 
+				}
+			}
+			else 
+				moveIndex = checkFree(ticTacToeBoard, chosenLetter, currentPlayer);
+			makeMove(ticTacToeBoard, chosenLetter, moveIndex);
+			System.out.println("\n--Updated board after the " + currentPlayer +"'s move--");
+			showBoard(ticTacToeBoard);
+			gameStatus = gameManager(ticTacToeBoard, chosenLetter);
+			chosenLetter = swapPlayerLetter(chosenLetter);
+			currentPlayer = swapPlayerTurn(currentPlayer);
+		} while(gameStatus.contains("change"));
+		if(gameStatus.contains("tie"))
+			System.out.println("It's a TIE!");
+		else
+			System.out.println("The game is WON by " + swapPlayerTurn(currentPlayer));
+	
 	public static void main(String[] args) {
 		
 		
